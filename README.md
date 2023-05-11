ed6906c84c42067072d40967f5752f032eeff6c3
Shallom Kanyori <shallomkanyori@gmail.com>
2023-05-11 10:02:41 +0000

    Add C program that is a simple UNIX command line interpreter
    Add a C program that interprets simple commands without arguments.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-Simple-Shell project
+## C - Simple Shell
+#### Write a simple UNIX command interpreter.
+
+**Task 0: Betty would be proud.**
+Write beautiful code that passes the Betty checks.
+
+**Task 1: Simple shell 0.1.**
+Write a UNIX command line interpreter.
+- Usage: `simple_shell`
+Your Shell should:
+- Display a prompt and wait for the user to type a command.
+- The prompt is displayed again each time a command has been executed.
+- The commands are simple: no semicolons, pipes, redirections or any other advanced features.
+- The commands are made only of one word. No arguments will be passed.
+- If an executable cannot be found, print an error message and display the promt again.
+- Handle errors.
+- Handle the "end of file" condition (`Ctrl-D`)
+You don't have to:
+- use the `PATH`
+- implement built-ins
+- handle special characters: `"`, `'`, `\`, `*`, `&`, `#`, backtick
+- be able to move the cursor
---
+++ main.c
@@ -0,0 +1,109 @@
+#include "main.h"
+
+/**
+ * print_error - displays an error message
+ * @av: the program arguments
+ *
+ * Return: nothing.
+ */
+void print_error(char **av)
+{
+	perror(av[0]);
+	fflush(stdout);
+}
+
+/**
+ * parse_input - parses input into a list containing the command name
+ * and NULL
+ * @input: the input
+ * @cmd: where to store the result
+ *
+ * Return: nothing.
+ */
+void parse_input(char *input, char **cmd)
+{
+	cmd[0] = strtok(input, "\n");
+	cmd[1] = NULL;
+}
+
+/**
+ * exec_cmd - executes the given command
+ * @cmd: a list containing the command name and NULL
+ * @av: the program arguments
+ * @env: the environment variables
+ *
+ * Return: 0 on success, non-zero on failure.
+ */
+int exec_cmd(char **cmd, char **av, char **env)
+{
+	int child_pid;
+	int status;
+
+	child_pid = fork();
+	if (child_pid == 0)
+	{
+		execve(cmd[0], cmd, env);
+		print_error(av); /* if execve returns, there is an error */
+		exit(errno);
+	}
+	else if (child_pid > 0)
+	{
+		wait(&status);
+		if (WIFEXITED(status))
+			return (WEXITSTATUS(status));
+		else
+			return (-1);
+	}
+	else
+	{
+		print_error(av);
+		return (errno);
+	}
+}
+
+/**
+ * main - A simple shell
+ * @ac: the number of program arguments
+ * @av: the program arguments
+ * @env: the environment variables
+ *
+ * Return: 0 on success, non-zero on error.
+ */
+int main(int ac, char **av, char **env)
+{
+	int interactive = isatty(STDIN_FILENO);
+	char *input = NULL;
+	size_t input_len = 0;
+	char *cmd[2]; /* handle one word arguments only for now */
+	int res;
+
+	(void) ac;
+	while (1)
+	{
+		if (interactive)
+		{
+			printf("$ ");
+			fflush(stdout);
+		}
+
+		res = getline(&input, &input_len, stdin);
+		if (res == -1)
+		{
+			if (!interactive || input_len > 0)
+				break;
+
+			print_error(av);
+			continue;
+		}
+
+		parse_input(input, cmd);
+		if (cmd[0] == NULL)
+			continue;
+
+		exec_cmd(cmd, av, env);
+	}
+
+	free(input);
+	input = NULL;
+	return (0);
+}
