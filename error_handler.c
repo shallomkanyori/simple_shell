b414c2d181e4a77ff1fc56b2d39ebbaa8b2c4e59
Shallom Kanyori <shallomkanyori@gmail.com>
2023-05-22 16:51:50 +0000

    Modify code to have the exact error output of sh

--- error_handler.c
+++ error_handler.c
@@ -2,14 +2,84 @@
 
 /**
  * print_error - displays an error message
- * @av: the program arguments
- * @res: a pointer to the return value of main
+ * @sh_data: a pointer to the structure containing the shell data
  *
  * Return: Nothing.
  */
-void print_error(char **av, int *res)
+void print_error(shdata_t *sh_data)
 {
-	*res = errno;
-	perror(av[0]);
-	fflush(stderr);
+	sh_data->res = errno;
+	if (errno != 1 && errno != 2)
+	{
+		put_str_err(sh_data->av[0]);
+		put_str_err(": ");
+		put_str_err(itoa(sh_data->line_num, sh_data->line_num_str));
+		put_str_err(": ");
+
+		if (sh_data->err_incl_cmd)
+		{
+			put_str_err(sh_data->cmd[0]);
+			put_str_err(": ");
+			sh_data->err_incl_cmd = 0;
+		}
+
+		if (sh_data->err_message != NULL)
+		{
+			put_str_err(sh_data->err_message);
+			put_chr_err('\n');
+			put_chr_err(BUFF_FLUSH);
+			sh_data->err_message = NULL;
+		}
+		else
+		{
+			put_chr_err(BUFF_FLUSH);
+			perror("");
+			fflush(stderr);
+		}
+	}
+	else
+	{
+		perror(sh_data->av[0]);
+		fflush(stderr);
+	}
+}
+
+/**
+ * put_str_err - write a string to stderr using a buffer
+ * @str: the string to write
+ *
+ * Return: -1 on error, 0 on success
+ */
+int put_str_err(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+		if (put_chr_err(str[i]) == -1)
+			return (-1);
+
+	return (0);
+}
+
+/**
+ * put_chr_err - writes a character to stderr using a buffer
+ * @c: the character
+ *
+ * Return: -1 on error, positive number on success.
+ */
+int put_chr_err(char c)
+{
+	static char buffer[BUFF_SIZE];
+	static int i;
+	int res = 1;
+
+	if (i == BUFF_SIZE || c == BUFF_FLUSH)
+	{
+		res = write(STDERR_FILENO, buffer, i);
+		i = 0;
+	}
+
+	if (c != BUFF_FLUSH)
+		buffer[i++] = c;
+	return (res);
 }
--- main.h
+++ main.h
@@ -9,14 +9,53 @@
 #include <sys/wait.h>
 #include <string.h>
 
-void print_error(char **av, int *res);
+#define BUFF_SIZE 1024
+#define BUFF_FLUSH -1
+#define INT_STR_SIZE 20
+
+/**
+ * struct shdata - A shell data struct
+ * @av: the program arguments
+ * @env: the environment variables
+ * @cmd: the array containing the current command and its arguments
+ * @input: the user input string
+ * @path: the value of the PATH environment variable
+ * @interactive: holds whether or not the shell is in interactive mode
+ * @line_num: the number of lines processed so far
+ * @line_num_str: holds the string conversion of line_num
+ * @res: the return value of main
+ * @err_message: holds custom shell error messages
+ * @err_incl_cmd: whether to include the command name in the custon error
+ * message
+ *
+ * Description: A struct that holds the relevant data of the shell
+ */
+typedef struct shdata
+{
+	char **av;
+	char **env;
+	char **cmd;
+	char *input;
+	char *path;
+	int interactive;
+	unsigned int line_num;
+	char line_num_str[INT_STR_SIZE];
+	int res;
+
+	char *err_message;
+	int err_incl_cmd;
+} shdata_t;
+
+/* error handling functions */
+void print_error(shdata_t *sh_data);
+int put_str_err(char *str);
+int put_chr_err(char c);
 
 /* exec functions */
-int handle_cmd(char **cmd, char **av, char **env, int *res);
-int exec_cmd(char **cmd, char **av, char **env, int *res);
+int handle_cmd(shdata_t *sh_data);
+int exec_cmd(shdata_t *sh_data, char *cmd_path);
 char *get_path(char **env);
-void search_path(char *path, char **full_path, char **cmd, char **av,
-		int *res);
+void search_path(shdata_t *sh_data, char **full_path, int *found);
 
 /* string functions */
 char *_strdup(char *str);
@@ -30,5 +69,8 @@ void *_memcpy(void *dest, void *src, unsigned int n);
 /* memory functions */
 void *_realloc(void *ptr, unsigned int s_old, unsigned int s_new);
 void free_ptr(void **ptr);
+
+/* util functions */
+char *itoa(unsigned int n, char *res);
 
 #endif /* MAIN_H */
